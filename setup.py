34770c5908c6997e5e28624c2fdd51dbb60ee4cd
Nafiul Araf <araf.cuet16@gmail.com>
2025-05-01 20:08:39 +0600

    project template partially ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Nafiul Araf",
+    author_email="cuetnafaraf@gmail.com",
+    packages=find_packages()
+)
