dbca1ce2d1f41da108dca8fa8c156a50fd58c74b
nafaraf <cuetnafaraf@gmail.com>
2025-05-02 23:48:15 +0600

    data ingestion done

--- demo.py
+++ demo.py
@@ -11,12 +11,16 @@
 
 
 # # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+# from src.logger import logging
+# from src.exception import MyException
+# import sys
 
-try:
-    a = 1 + 's'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+# try:
+#     a = 1 + 's'
+# except Exception as e:
+#     logging.info(e)
+#     raise MyException(e, sys) from e
+
+from src.pipline.training_pipeline import TrainPipeline
+pipeline = TrainPipeline()
+pipeline.run_pipeline()
